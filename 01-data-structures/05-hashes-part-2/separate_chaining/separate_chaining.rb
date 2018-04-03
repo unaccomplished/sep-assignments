2ad12392781c8266fd6894fba3c0672e69564a29
Jocelyn Hsu <hsu.joce@gmail.com>
2018-04-03 11:18:32 -0700

    In progress on separate_chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,9 +5,24 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
+    @item_count = 0
   end
 
   def []=(key, value)
+    node = Node.new(key, value)
+    i = index(key, size)
+    if @items[i].nil?
+      list = LinkedList.new
+      list.add_to_tail(node)
+      @items[i] = list
+    else
+      @items[i].add_to_tail(node)
+    end
+    @item_count += 1
+    if load_factor >= @max_load_factor
+      # resize
+    end
   end
 
   def [](key)
@@ -17,17 +32,25 @@ class SeparateChaining
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @item_count / self.size.to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp = @items.compact
+    @items = Array.new(self.size * 2)
+    temp.each do |item|
+      self[item.key] = item.value
+    end
   end
 end
