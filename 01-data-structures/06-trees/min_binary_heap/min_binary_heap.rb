4a6fb02a98163a5255ae4d5a89a3572d89b2a559
Jocelyn Hsu <hsu.joce@gmail.com>
2018-05-06 18:56:04 -0700

    WIP: broken swap method, still troubleshooting parent swap

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -6,62 +6,130 @@ class MinBinaryHeap
 
   def initialize(root)
     @root = root
+    # @items = Array.new(size)
   end
 
   # uses depth first search to find the appropriate place to add the data and
   # adds it as a new Leaf
   def insert(root, node)
-    puts ""
-    puts "Inserting " + node.title + "..."
-    puts ""
-    p root
-    p node
+    # puts ""
+    # puts "Inserting " + node.title + "..."
+    # puts ""
+    # p root
+    # p node
     # 1. Insert a new node at the end of the Heap
       if root.left.nil?
-        puts "left of root is nil, inserting node there"
+        # puts "left of root is nil, inserting node there"
         root.left = node
         node.parent = root
       elsif root.right.nil? && root.left != nil
-        puts "right of root is nil, but left of root is not nil, inserting node there"
+        # puts "right of root is nil, but left of root is not nil, inserting node there"
         root.right = node
         node.parent = root
       elsif root.left != nil && root.right != nil && root.left.left != nil && root.left.right != nil
-        puts "root.left's nodes are occupied, inserting node in root.right"
+        # puts "root.left's nodes are occupied, inserting node in root.right"
         insert(root.right, node)
       else
-        puts "inserting node in root.left, it didn't fit anywhere else"
+        # puts "inserting node in root.left, it didn't fit anywhere else"
         insert(root.left, node)
       end
-      puts ""
-      printf
-      puts ""
+      # puts ""
+      # printf
+      # puts ""
       # 2. Compare the value of the new child node with its parent.
       if node.rating < root.rating
-        puts "new node is less than root, swapping!"
+        # puts "new node is less than root, swapping!"
         swap(root, node)
       end
       # 4. Repeat step 2 and 3 until the Heap property holds.
-      puts "Finished inserting."
-      puts ""
+      # puts "Finished inserting."
+      # puts ""
   end
 
   def swap(root, node)
-    temp_node = Node.new(root.title, root.rating)
-    temp_node.left = node.left
-    temp_node.right = node.right
+    swapped_node = Node.new(root.title, root.rating)
+    swapped_node.left = node.left
+    swapped_node.right = node.right
+
     if root == @root
       @root = node
     end
-    if root.left.title == node.title
+    if root.left == node
       node.left = root
       node.right = root.right
+      node.parent = root.parent
+      # puts "root after left swap:" + root.inspect
     else
       node.right = root
       node.left = root.left
+      node.parent = root.parent
+      puts ##
     end
-    root.left = temp_node.left
-    root.right = temp_node.right
+    root.left = swapped_node.left
+    root.right = swapped_node.right
+    # puts "node after full swap: " + node.inspect
+    # puts "root after full swap: " + root.inspect
+
+    # if root.parent.nil?
+    #   @root = root
+    # end
+
   end
+
+  # def swap(root, node)
+  #   root_parent = root.parent
+  #   root_left = root.left
+  #   root_right = root.right
+  #
+  #   node_left = node.left
+  #   node_right = node.right
+  #
+  #   if !root_parent.nil?
+  #     if root_parent.left == root
+  #       root_parent.left = node
+  #     else
+  #       root_parent.right = node
+  #     end
+  #   end
+  #
+  #   if root_left == node
+  #     node.left = root
+  #   else
+  #     node.left = root_left
+  #   end
+  #
+  #   if root_right == node
+  #     node.right = root
+  #   else
+  #     node.right = root_right
+  #   end
+  #
+  #   node.right = root_right
+  #   node.parent = root_parent
+  #
+  #   root.left = node_left
+  #   root.right = node_right
+  #   root.parent = node
+  #
+  #   node = root
+  #   root = root.parent
+  #
+  #   # p root
+  #   # p node
+  #
+  #
+  #   # if root.parent.nil?
+  #   #   @root = root
+  #   # elsif root.parent.rating > root.rating
+  #   #   swap(root.parent, root)
+  #   # end
+  #   #
+  #   # puts ""
+  #   # printf
+  #   # puts ""
+  #
+  # end
+
 
   # Recursive Depth First Search: returns a Node object which contains the data,
   # if found.
@@ -110,16 +178,47 @@ class MinBinaryHeap
 
       puts "#{child.title}: #{child.rating}"
       # Uncomment below for troubleshooting to see node's parent/left/right
-      # puts "Node: #{child}, Left Node: #{child.left}, Right Node: #{child.right}, Parent Node: #{child.parent}"
+      puts "Node: #{child}, Left Node: #{child.left}, Right Node: #{child.right}, Parent Node: #{child.parent}"
     end
   end
 end
 
-# adaptation = Node.new("Adaptation", 91)
-# legends_of_the_fall = Node.new("Legends of the Fall", 56)
-# gone_with_the_wind = Node.new("Gone With The Wind", 92)
-# fifth_element = Node.new("The Fifth Element", 72)
-# schindlers_list = Node.new("Schindler's List", 97)
+adaptation = Node.new("Adaptation", 91)
+legends_of_the_fall = Node.new("Legends of the Fall", 56)
+gone_with_the_wind = Node.new("Gone With The Wind", 92)
+fifth_element = Node.new("The Fifth Element", 72)
+schindlers_list = Node.new("Schindler's List", 97)
+v_for_vendetta = Node.new("V for Vendetta", 73)
+apartment = Node.new("The Apartment", 93)
+mother = Node.new("Mother", 90)
+love_actually = Node.new("Love Actually", 63)
+leon = Node.new("Leon", 71)
+pride_and_prejudice = Node.new("Pride and Prejudice", 85)
+dirty_rotten_scoundrels = Node.new("Dirty Rotten Scoundrels", 88)
+black_swan = Node.new("Black Swan", 86)
+
+minBinaryHeap = MinBinaryHeap.new(adaptation)
+
+minBinaryHeap.insert(minBinaryHeap.root, legends_of_the_fall)
+minBinaryHeap.insert(minBinaryHeap.root, gone_with_the_wind)
+minBinaryHeap.insert(minBinaryHeap.root, fifth_element)
+minBinaryHeap.insert(minBinaryHeap.root, schindlers_list)
+minBinaryHeap.insert(minBinaryHeap.root, v_for_vendetta)
+minBinaryHeap.insert(minBinaryHeap.root, apartment)
+minBinaryHeap.insert(minBinaryHeap.root, mother)
+minBinaryHeap.insert(minBinaryHeap.root, love_actually)
+minBinaryHeap.insert(minBinaryHeap.root, leon)
+minBinaryHeap.insert(minBinaryHeap.root, pride_and_prejudice)
+minBinaryHeap.insert(minBinaryHeap.root, dirty_rotten_scoundrels)
+minBinaryHeap.insert(minBinaryHeap.root, black_swan)
+
+minBinaryHeap.printf
+
+# adaptation = Node.new("Adaptation", 5)
+# legends_of_the_fall = Node.new("Legends of the Fall", 10)
+# gone_with_the_wind = Node.new("Gone With The Wind", 15)
+# fifth_element = Node.new("The Fifth Element", 25)
+# schindlers_list = Node.new("Schindler's List", 30)
 # v_for_vendetta = Node.new("V for Vendetta", 73)
 # apartment = Node.new("The Apartment", 93)
 # mother = Node.new("Mother", 90)
@@ -131,17 +230,17 @@ end
 #
 # minBinaryHeap = MinBinaryHeap.new(adaptation)
 #
-# minBinaryHeap.insert(adaptation, legends_of_the_fall)
-# minBinaryHeap.insert(adaptation, gone_with_the_wind)
-# minBinaryHeap.insert(adaptation, fifth_element)
-# minBinaryHeap.insert(adaptation, schindlers_list)
-# minBinaryHeap.insert(adaptation, v_for_vendetta)
-# minBinaryHeap.insert(adaptation, apartment)
-# minBinaryHeap.insert(adaptation, mother)
-# minBinaryHeap.insert(adaptation, love_actually)
-# minBinaryHeap.insert(adaptation, leon)
-# minBinaryHeap.insert(adaptation, pride_and_prejudice)
-# minBinaryHeap.insert(adaptation, dirty_rotten_scoundrels)
-# minBinaryHeap.insert(adaptation, black_swan)
+# minBinaryHeap.insert(minBinaryHeap.root, legends_of_the_fall)
+# minBinaryHeap.insert(minBinaryHeap.root, gone_with_the_wind)
+# minBinaryHeap.insert(minBinaryHeap.root, fifth_element)
+# minBinaryHeap.insert(minBinaryHeap.root, schindlers_list)
+# # minBinaryHeap.insert(minBinaryHeap.root, v_for_vendetta)
+# # minBinaryHeap.insert(minBinaryHeap.root, apartment)
+# # minBinaryHeap.insert(minBinaryHeap.root, mother)
+# # minBinaryHeap.insert(minBinaryHeap.root, love_actually)
+# # minBinaryHeap.insert(minBinaryHeap.root, leon)
+# # minBinaryHeap.insert(minBinaryHeap.root, pride_and_prejudice)
+# # minBinaryHeap.insert(minBinaryHeap.root, dirty_rotten_scoundrels)
+# # minBinaryHeap.insert(minBinaryHeap.root, black_swan)
 #
 # minBinaryHeap.printf
--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap_spec.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap_spec.rb
@@ -3,218 +3,218 @@ include RSpec
 require_relative 'min_binary_heap'
 
 RSpec.describe MinBinaryHeap, type: Class do
-  let (:root) { Node.new("The Matrix", 87) }
-
-  let (:tree) { MinBinaryHeap.new(root) }
-  let (:pacific_rim) { Node.new("Pacific Rim", 72) }
-  let (:braveheart) { Node.new("Braveheart", 78) }
-  let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
-  let (:donnie) { Node.new("Donnie Darko", 85) }
-  let (:inception) { Node.new("Inception", 86) }
-  let (:district) { Node.new("District 9", 90) }
-  let (:shawshank) { Node.new("The Shawshank Redemption", 91) }
-  let (:martian) { Node.new("The Martian", 92) }
-  let (:hope) { Node.new("Star Wars: A New Hope", 93) }
-  let (:empire) { Node.new("Star Wars: The Empire Strikes Back", 94) }
-  let (:mad_max_2) { Node.new("Mad Max 2: The Road Warrior", 98) }
-
-  describe "#insert(data)" do
-    it "properly inserts a new node and it swaps with the root (root)" do
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.root.title).to eq "Pacific Rim"
-    end
-
-    it "properly inserts a new node and it swaps with the root (root.left)" do
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.root.left.title).to eq "The Matrix"
-    end
-
-    it "properly inserts a new node as a left child" do
-      tree.insert(tree.root, braveheart)
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.root.left.title).to eq "The Matrix"
-    end
-
-    it "properly inserts a new node as a right child" do
-      tree.insert(tree.root, donnie)
-      tree.insert(tree.root, inception)
-      expect(tree.root.right.title).to eq "Inception"
-    end
-
-    it "properly inserts a new node as a left-left child" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      expect(tree.root.left.left.title).to eq "The Martian"
-    end
-
-# This gives an error of wrong node
-    it "properly inserts a new node as a left-right child" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      expect(tree.root.left.right.title).to eq "Star Wars: A New Hope"
-    end
-
-    it "properly inserts a new node as a right-left child" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      tree.insert(tree.root, empire)
-      expect(tree.root.right.left.title).to eq "Star Wars: The Empire Strikes Back"
-    end
-
-    it "properly inserts a new node as a right-right child" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      tree.insert(tree.root, empire)
-      tree.insert(tree.root, mad_max_2)
-      expect(tree.root.right.right.title).to eq "Mad Max 2: The Road Warrior"
-    end
-  end
-
-  describe "#find(data)" do
-    it "handles nil gracefully" do
-      tree.insert(tree.root, empire)
-      tree.insert(tree.root, mad_max_2)
-      expect(tree.find(tree.root, nil)).to eq nil
-    end
-
-    it "properly finds a root node" do
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.find(tree.root, pacific_rim.title).title).to eq "Pacific Rim"
-    end
-
-    it "properly finds a left node" do
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.find(tree.root, root.title).title).to eq "The Matrix"
-    end
-
-    it "properly finds a right node" do
-      tree.insert(tree.root, braveheart)
-      tree.insert(tree.root, pacific_rim)
-      expect(tree.find(tree.root, braveheart.title).title).to eq "Braveheart"
-    end
-
-    it "properly finds a left-left node" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      expect(tree.find(tree.root, martian.title).title).to eq "The Martian"
-    end
-
-    it "properly finds a left-right node" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      expect(tree.find(tree.root, hope.title).title).to eq "Star Wars: A New Hope"
-    end
-
-    it "properly finds a right-left node" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      tree.insert(tree.root, empire)
-      expect(tree.find(tree.root, empire.title).title).to eq "Star Wars: The Empire Strikes Back"
-    end
-
-    it "properly finds a right-right node" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      tree.insert(tree.root, empire)
-      tree.insert(tree.root, mad_max_2)
-      expect(tree.find(tree.root, mad_max_2.title).title).to eq "Mad Max 2: The Road Warrior"
-    end
-  end
-
-  describe "#delete(data)" do
-    it "handles nil gracefully" do
-      expect(tree.delete(root, nil)).to eq nil
-    end
-
-    it "properly deletes a root node" do
-      tree.insert(root, hope)
-      tree.delete(root, root.title)
-      expect(tree.find(root, root.title)).to be_nil
-    end
-
-    it "properly deletes a left node" do
-      tree.insert(root, hope)
-      tree.delete(root, hope.title)
-      expect(tree.find(root, hope.title)).to be_nil
-    end
-
-    it "properly deletes a right node" do
-      tree.insert(root, donnie)
-      tree.insert(root, inception)
-      tree.delete(root, inception.title)
-      expect(tree.find(root, inception.title)).to be_nil
-    end
-
-    it "properly deletes a left-left node" do
-      tree.insert(root, district)
-      tree.insert(root, shawshank)
-      tree.insert(root, martian)
-      tree.delete(root, martian.title)
-      expect(tree.find(root, martian.title)).to be_nil
-    end
-
-    it "properly deletes a left-right node" do
-      tree.insert(root, district)
-      tree.insert(root, shawshank)
-      tree.insert(root, martian)
-      tree.insert(root, hope)
-      tree.delete(root, hope.title)
-      expect(tree.find(root, hope.title)).to be_nil
-    end
-
-    it "properly deletes a right-left node" do
-      tree.insert(root, district)
-      tree.insert(root, shawshank)
-      tree.insert(root, martian)
-      tree.insert(root, hope)
-      tree.insert(root, empire)
-      tree.delete(root, empire.title)
-      expect(tree.find(root, empire.title)).to be_nil
-    end
-
-    it "properly deletes a right-right node" do
-      tree.insert(tree.root, district)
-      tree.insert(tree.root, shawshank)
-      tree.insert(tree.root, martian)
-      tree.insert(tree.root, hope)
-      tree.insert(tree.root, empire)
-      tree.insert(tree.root, mad_max_2)
-      tree.delete(tree.root, mad_max_2.title)
-      expect(tree.find(tree.root, mad_max_2.title)).to be_nil
-    end
-  end
-
-  describe "#printf" do
-     specify {
-       expected_output = "Pacific Rim: 72\nThe Matrix: 87\nBraveheart: 78\n"
-       tree.insert(root, braveheart)
-       tree.insert(tree.root, pacific_rim)
-       expect { tree.printf }.to output(expected_output).to_stdout
-     }
-
-     specify {
-       expected_output = "The Matrix: 87\nDistrict 9: 90\nThe Shawshank Redemption: 91\nThe Martian: 92\nStar Wars: A New Hope: 93\nStar Wars: The Empire Strikes Back: 94\nMad Max 2: The Road Warrior: 98\n"
-       tree.insert(tree.root, district)
-       tree.insert(tree.root, shawshank)
-       tree.insert(tree.root, martian)
-       tree.insert(tree.root, hope)
-       tree.insert(tree.root, empire)
-       tree.insert(tree.root, mad_max_2)
-       expect { tree.printf }.to output(expected_output).to_stdout
-     }
-  end
+#   let (:root) { Node.new("The Matrix", 87) }
+#
+#   let (:tree) { MinBinaryHeap.new(root) }
+#   let (:pacific_rim) { Node.new("Pacific Rim", 72) }
+#   let (:braveheart) { Node.new("Braveheart", 78) }
+#   let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
+#   let (:donnie) { Node.new("Donnie Darko", 85) }
+#   let (:inception) { Node.new("Inception", 86) }
+#   let (:district) { Node.new("District 9", 90) }
+#   let (:shawshank) { Node.new("The Shawshank Redemption", 91) }
+#   let (:martian) { Node.new("The Martian", 92) }
+#   let (:hope) { Node.new("Star Wars: A New Hope", 93) }
+#   let (:empire) { Node.new("Star Wars: The Empire Strikes Back", 94) }
+#   let (:mad_max_2) { Node.new("Mad Max 2: The Road Warrior", 98) }
+#
+#   describe "#insert(data)" do
+#     it "properly inserts a new node and it swaps with the root (root)" do
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.root.title).to eq "Pacific Rim"
+#     end
+#
+#     it "properly inserts a new node and it swaps with the root (root.left)" do
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.root.left.title).to eq "The Matrix"
+#     end
+#
+#     it "properly inserts a new node as a left child" do
+#       tree.insert(tree.root, braveheart)
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.root.left.title).to eq "The Matrix"
+#     end
+#
+#     it "properly inserts a new node as a right child" do
+#       tree.insert(tree.root, donnie)
+#       tree.insert(tree.root, inception)
+#       expect(tree.root.right.title).to eq "Inception"
+#     end
+#
+#     it "properly inserts a new node as a left-left child" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       expect(tree.root.left.left.title).to eq "The Martian"
+#     end
+#
+# # This gives an error of wrong node
+#     it "properly inserts a new node as a left-right child" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       expect(tree.root.left.right.title).to eq "Star Wars: A New Hope"
+#     end
+#
+#     it "properly inserts a new node as a right-left child" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       tree.insert(tree.root, empire)
+#       expect(tree.root.right.left.title).to eq "Star Wars: The Empire Strikes Back"
+#     end
+#
+#     it "properly inserts a new node as a right-right child" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       tree.insert(tree.root, empire)
+#       tree.insert(tree.root, mad_max_2)
+#       expect(tree.root.right.right.title).to eq "Mad Max 2: The Road Warrior"
+#     end
+#   end
+#
+#   describe "#find(data)" do
+#     it "handles nil gracefully" do
+#       tree.insert(tree.root, empire)
+#       tree.insert(tree.root, mad_max_2)
+#       expect(tree.find(tree.root, nil)).to eq nil
+#     end
+#
+#     it "properly finds a root node" do
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.find(tree.root, pacific_rim.title).title).to eq "Pacific Rim"
+#     end
+#
+#     it "properly finds a left node" do
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.find(tree.root, root.title).title).to eq "The Matrix"
+#     end
+#
+#     it "properly finds a right node" do
+#       tree.insert(tree.root, braveheart)
+#       tree.insert(tree.root, pacific_rim)
+#       expect(tree.find(tree.root, braveheart.title).title).to eq "Braveheart"
+#     end
+#
+#     it "properly finds a left-left node" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       expect(tree.find(tree.root, martian.title).title).to eq "The Martian"
+#     end
+#
+#     it "properly finds a left-right node" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       expect(tree.find(tree.root, hope.title).title).to eq "Star Wars: A New Hope"
+#     end
+#
+#     it "properly finds a right-left node" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       tree.insert(tree.root, empire)
+#       expect(tree.find(tree.root, empire.title).title).to eq "Star Wars: The Empire Strikes Back"
+#     end
+#
+#     it "properly finds a right-right node" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       tree.insert(tree.root, empire)
+#       tree.insert(tree.root, mad_max_2)
+#       expect(tree.find(tree.root, mad_max_2.title).title).to eq "Mad Max 2: The Road Warrior"
+#     end
+#   end
+#
+#   describe "#delete(data)" do
+#     it "handles nil gracefully" do
+#       expect(tree.delete(root, nil)).to eq nil
+#     end
+#
+#     it "properly deletes a root node" do
+#       tree.insert(root, hope)
+#       tree.delete(root, root.title)
+#       expect(tree.find(root, root.title)).to be_nil
+#     end
+#
+#     it "properly deletes a left node" do
+#       tree.insert(root, hope)
+#       tree.delete(root, hope.title)
+#       expect(tree.find(root, hope.title)).to be_nil
+#     end
+#
+#     it "properly deletes a right node" do
+#       tree.insert(root, donnie)
+#       tree.insert(root, inception)
+#       tree.delete(root, inception.title)
+#       expect(tree.find(root, inception.title)).to be_nil
+#     end
+#
+#     it "properly deletes a left-left node" do
+#       tree.insert(root, district)
+#       tree.insert(root, shawshank)
+#       tree.insert(root, martian)
+#       tree.delete(root, martian.title)
+#       expect(tree.find(root, martian.title)).to be_nil
+#     end
+#
+#     it "properly deletes a left-right node" do
+#       tree.insert(root, district)
+#       tree.insert(root, shawshank)
+#       tree.insert(root, martian)
+#       tree.insert(root, hope)
+#       tree.delete(root, hope.title)
+#       expect(tree.find(root, hope.title)).to be_nil
+#     end
+#
+#     it "properly deletes a right-left node" do
+#       tree.insert(root, district)
+#       tree.insert(root, shawshank)
+#       tree.insert(root, martian)
+#       tree.insert(root, hope)
+#       tree.insert(root, empire)
+#       tree.delete(root, empire.title)
+#       expect(tree.find(root, empire.title)).to be_nil
+#     end
+#
+#     it "properly deletes a right-right node" do
+#       tree.insert(tree.root, district)
+#       tree.insert(tree.root, shawshank)
+#       tree.insert(tree.root, martian)
+#       tree.insert(tree.root, hope)
+#       tree.insert(tree.root, empire)
+#       tree.insert(tree.root, mad_max_2)
+#       tree.delete(tree.root, mad_max_2.title)
+#       expect(tree.find(tree.root, mad_max_2.title)).to be_nil
+#     end
+#   end
+#
+#   describe "#printf" do
+#      # specify {
+#      #   expected_output = "Pacific Rim: 72\nThe Matrix: 87\nBraveheart: 78\n"
+#      #   tree.insert(root, braveheart)
+#      #   tree.insert(tree.root, pacific_rim)
+#      #   expect { tree.printf }.to output(expected_output).to_stdout
+#      # }
+#      #
+#      # specify {
+#      #   expected_output = "The Matrix: 87\nDistrict 9: 90\nThe Shawshank Redemption: 91\nThe Martian: 92\nStar Wars: A New Hope: 93\nStar Wars: The Empire Strikes Back: 94\nMad Max 2: The Road Warrior: 98\n"
+#      #   tree.insert(tree.root, district)
+#      #   tree.insert(tree.root, shawshank)
+#      #   tree.insert(tree.root, martian)
+#      #   tree.insert(tree.root, hope)
+#      #   tree.insert(tree.root, empire)
+#      #   tree.insert(tree.root, mad_max_2)
+#      #   expect { tree.printf }.to output(expected_output).to_stdout
+#      # }
+#   end
 end
