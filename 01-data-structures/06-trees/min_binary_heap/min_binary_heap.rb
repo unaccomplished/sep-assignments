a9a91fc39a74b7d5a72a29eda6a71e968d3d9475
Jocelyn Hsu <hsu.joce@gmail.com>
2018-05-12 16:59:07 -0700

    All tests pass

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -6,7 +6,6 @@ class MinBinaryHeap
 
   def initialize(root)
     @root = root
-    # @items = Array.new(size)
   end
 
   # uses depth first search to find the appropriate place to add the data and
@@ -46,89 +45,60 @@ class MinBinaryHeap
       # puts ""
   end
 
+
   def swap(root, node)
-    swapped_node = Node.new(root.title, root.rating)
-    swapped_node.left = node.left
-    swapped_node.right = node.right
+    root_parent = root.parent
+    root_left = root.left
+    root_right = root.right
 
-    if root == @root
-      @root = node
+    node_left = node.left
+    node_right = node.right
+
+    if !root_parent.nil?
+      if root_parent.left == root
+        root_parent.left = node
+      else
+        root_parent.right = node
+      end
     end
-    if root.left == node
+
+    if root_left == node
       node.left = root
-      node.right = root.right
-      node.parent = root.parent
-      # puts "root after left swap:" + root.inspect
     else
-      node.right = root
-      node.left = root.left
-      node.parent = root.parent
-      puts ##
+      node.left = root_left
     end
-    root.left = swapped_node.left
-    root.right = swapped_node.right
-    # puts "node after full swap: " + node.inspect
-    # puts "root after full swap: " + root.inspect
 
-    # if root.parent.nil?
-    #   @root = root
-    # end
+    if root_right == node
+      node.right = root
+    else
+      node.right = root_right
+    end
+
+    node.parent = root_parent
+
+    root.left = node_left
+    root.right = node_right
+    root.parent = node
+
+    node = root
+    root = root.parent
+
+    # p root
+    # p node
+    # puts ""
+
+
+    if root.parent.nil?
+      @root = root
+    elsif root.parent.rating > root.rating
+      swap(root.parent, root)
+    end
+    #
+    # puts ""
+    # printf
+    # puts ""
 
   end
-
-  # def swap(root, node)
-  #   root_parent = root.parent
-  #   root_left = root.left
-  #   root_right = root.right
-  #
-  #   node_left = node.left
-  #   node_right = node.right
-  #
-  #   if !root_parent.nil?
-  #     if root_parent.left == root
-  #       root_parent.left = node
-  #     else
-  #       root_parent.right = node
-  #     end
-  #   end
-  #
-  #   if root_left == node
-  #     node.left = root
-  #   else
-  #     node.left = root_left
-  #   end
-  #
-  #   if root_right == node
-  #     node.right = root
-  #   else
-  #     node.right = root_right
-  #   end
-  #
-  #   node.right = root_right
-  #   node.parent = root_parent
-  #
-  #   root.left = node_left
-  #   root.right = node_right
-  #   root.parent = node
-  #
-  #   node = root
-  #   root = root.parent
-  #
-  #   # p root
-  #   # p node
-  #
-  #
-  #   # if root.parent.nil?
-  #   #   @root = root
-  #   # elsif root.parent.rating > root.rating
-  #   #   swap(root.parent, root)
-  #   # end
-  #   #
-  #   # puts ""
-  #   # printf
-  #   # puts ""
-  #
-  # end
 
 
   # Recursive Depth First Search: returns a Node object which contains the data,
@@ -178,7 +148,7 @@ class MinBinaryHeap
 
       puts "#{child.title}: #{child.rating}"
       # Uncomment below for troubleshooting to see node's parent/left/right
-      puts "Node: #{child}, Left Node: #{child.left}, Right Node: #{child.right}, Parent Node: #{child.parent}"
+      # puts "Node: #{child}, Left Node: #{child.left}, Right Node: #{child.right}, Parent Node: #{child.parent}"
     end
   end
 end
@@ -213,34 +183,3 @@ minBinaryHeap.insert(minBinaryHeap.root, dirty_rotten_scoundrels)
 minBinaryHeap.insert(minBinaryHeap.root, black_swan)
 
 minBinaryHeap.printf
-
-# adaptation = Node.new("Adaptation", 5)
-# legends_of_the_fall = Node.new("Legends of the Fall", 10)
-# gone_with_the_wind = Node.new("Gone With The Wind", 15)
-# fifth_element = Node.new("The Fifth Element", 25)
-# schindlers_list = Node.new("Schindler's List", 30)
-# v_for_vendetta = Node.new("V for Vendetta", 73)
-# apartment = Node.new("The Apartment", 93)
-# mother = Node.new("Mother", 90)
-# love_actually = Node.new("Love Actually", 63)
-# leon = Node.new("Leon", 71)
-# pride_and_prejudice = Node.new("Pride and Prejudice", 85)
-# dirty_rotten_scoundrels = Node.new("Dirty Rotten Scoundrels", 88)
-# black_swan = Node.new("Black Swan", 86)
-#
-# minBinaryHeap = MinBinaryHeap.new(adaptation)
-#
-# minBinaryHeap.insert(minBinaryHeap.root, legends_of_the_fall)
-# minBinaryHeap.insert(minBinaryHeap.root, gone_with_the_wind)
-# minBinaryHeap.insert(minBinaryHeap.root, fifth_element)
-# minBinaryHeap.insert(minBinaryHeap.root, schindlers_list)
-# # minBinaryHeap.insert(minBinaryHeap.root, v_for_vendetta)
-# # minBinaryHeap.insert(minBinaryHeap.root, apartment)
-# # minBinaryHeap.insert(minBinaryHeap.root, mother)
-# # minBinaryHeap.insert(minBinaryHeap.root, love_actually)
-# # minBinaryHeap.insert(minBinaryHeap.root, leon)
-# # minBinaryHeap.insert(minBinaryHeap.root, pride_and_prejudice)
-# # minBinaryHeap.insert(minBinaryHeap.root, dirty_rotten_scoundrels)
-# # minBinaryHeap.insert(minBinaryHeap.root, black_swan)
-#
-# minBinaryHeap.printf
